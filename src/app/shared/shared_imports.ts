b961b219ff7276af667d03a44261b2d508b40e6b
ValeriaRojo <rh202354373@alm.buap.mx>
2026-03-22 23:08:13 -0600

    Archivo de importaciones editado

--- src/app/shared/shared_imports.ts
+++ src/app/shared/shared_imports.ts
@@ -2,13 +2,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import {MatButtonModule} from '@angular/material/button';
+import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 
 export const SHARED_IMPORTS = [
     CommonModule,
     FormsModule,
+
     MatButtonModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    
 ] as const;
