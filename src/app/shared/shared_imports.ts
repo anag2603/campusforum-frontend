919e5e85a043377a281c9fdfd33cb0de0323cb90
ValeriaRojo <rh202354373@alm.buap.mx>
2026-04-15 19:46:49 -0600

    Cambios en el archivo shared_imports

--- src/app/shared/shared_imports.ts
+++ src/app/shared/shared_imports.ts
@@ -1,31 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatRadioModule } from '@angular/material/radio';
+import { _MatInternalFormField } from '@angular/material/core';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCheckbox, MatCheckboxModule} from '@angular/material/checkbox';
-import {_MatInternalFormField} from '@angular/material/core';
+import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
 import {MatDividerModule} from '@angular/material/divider';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
-import {MatFormField, MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 export const SHARED_IMPORTS = [
     CommonModule,
     FormsModule,
 
-    MatFormFieldModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
     MatInputModule,
     MatSelectModule,
     MatCheckboxModule,
+    MatCardModule,
+    MatDrawer,
+    MatDrawerContainer,
+    MatDrawerContent,
     MatToolbarModule,
-    MatSidenavModule,
-    MatRadioModule, //Import nuevo
-    
 ] as const;
